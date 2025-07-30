1875a7043c9fdbb3de6b56229c860f0a39ed2afc
Avijit07x <deyavijit134@gmail.com>
2025-07-30 23:50:55 +0530

    refactor : simplify error component by removing unused props type

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,12 +2,10 @@
 
 import React from "react";
 
-type Props = {};
-
-const error: React.FC<Props> = () => {
+const error: React.FC = () => {
 	return (
 		<div className="from-bgDark to-bgDark flex min-h-screen flex-col items-center justify-center bg-gradient-to-br via-zinc-900 text-white">
-			<h1>Error Page</h1>
+			<h1 className="text-base">Error Page</h1>
 			<p>Something went wrong. Please try again later.</p>
 			<p>If the problem persists, contact support.</p>
 			<p>Thank you for your patience.</p>
